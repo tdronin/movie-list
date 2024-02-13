8ff8a2d0aa95ef2f51879ba3f939403a464cc422
tommymdaniel <tommymdaniel@gmail.com>
2024-02-13 04:44:33 -0500

    part one complete

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+require('dotenv').config({ path: path.resolve(__dirname, 'ENV', '.env') });
+const PORT = 4000;
+
+app.use(express.json());
+
+// const movies = [
+//   { title: 'Mean Girls' },
+//   { title: 'Hackers' },
+//   { title: 'The Grey' },
+//   { title: 'Sunshine' },
+//   { title: 'Ex Machina' },
+// ];
+
+// app.get('/movies', (req, res) => {
+//   res.json(movies);
+// });
+
+
+// Setup connection to PostgreSQL
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
+app.use(express.json());
+
+// Endpoint to fetch movies from PostgreSQL database
+app.get('/movies', async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM movies');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
